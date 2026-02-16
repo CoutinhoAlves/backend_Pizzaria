1886d100998526b2135f6d9ca06bd9efd9de5338
COUTINHO ALVES <couto.alves@outlook.com>
2026-02-16 00:32:36 -0300

    inicializando o projeto pelo backend

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import cors from 'cors';
+import "dotenv/config";
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
